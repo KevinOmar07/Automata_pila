9c63e150965e36da2df1678cc6ccd492d912220c
Kevin O <Kevinnuricumbo@gmail.com>
2022-03-29 10:29:31 -0600

    actualizado

--- src/main/java/com/example/structure_automata_pila/Automata_pila.java
+++ src/main/java/com/example/structure_automata_pila/Automata_pila.java
@@ -1,14 +1,28 @@
 package com.example.structure_automata_pila;
 
+import java.util.ArrayList;
 import java.util.Stack;
 import java.util.regex.Pattern;
+
+/*
+
+struct prueba {
+	name:string,
+}
+
+
+ */
 
 public class Automata_pila {
 
     private boolean status = true;
-    private String entrada = "";
+    private ArrayList <String> entrada;
+    Pattern expresionNum = Pattern.compile("(^[0-9]{1})");
+    Pattern expresionLetra = Pattern.compile("(^[a-z]{1})");
+    String aAux;
+    boolean auxPun = true;
 
-    Automata_pila(String entrada ){
+    Automata_pila(ArrayList <String> entrada ){
         this.entrada = entrada;
     }
 
@@ -22,36 +36,58 @@ public class Automata_pila {
         pila.push("S");
         String x = "";
 
-        String a = entrada.substring(0,6);
         int con = 0;
-
+        int apuntador = 0;
+        int apuntadorAux = 0;
+        char [] cadenaAux;
+        String a = entrada.get(apuntador);
         do {
+            mostrar_pila(pila);
             con++;
             System.out.println("Vuelta " + con);
             x = pila.get(pila.size()-1);
             System.out.println(x);
-            System.out.println(a + "\n");
+            System.out.println(a + " : a\n");
             if (terminales.matcher(x).find()){
                 System.out.println("Es un terminal: " + x);
-                if(x.equals(a)){
+                if(x.equals(a) || x.equals(aAux)){
                     pila.pop();
-                    mover_apuntador(entrada);
+
+                    /*if(apuntador > 0){
+                        cadenaAux = entrada.get(apuntador).toCharArray();
+                        a = String.valueOf(cadenaAux[apuntadorAux]);
+                        apuntadorAux++;
+                    } else {
+
+                    }*/
+                    if (auxPun){
+                        apuntador++;
+                        a = entrada.get(apuntador);
+                    }
+
                 } else {
                     System.out.println("error");
-                    pila.clear();
+                    status = false;
                 }
             } else {
                 System.out.println("No es un terminal");
-                pila = meter_produccion(pila);
+                pila = meter_produccion(pila, a);
             }
-        }while (!pila.isEmpty());
+        }while (!pila.isEmpty() && status);
+
+        if(pila.isEmpty()){
+            System.out.println("Entrada correcta: " + pila.isEmpty());
+        }
     }
 
-    private void mover_apuntador(String entrada){
+    /*private String mover_apuntador(ArrayList <String> entrada){
         System.out.println("Se mueve el puntero");
-    }
+        entrada = entrada.substring(ini,fin);
 
-    private Stack<String> meter_produccion(Stack<String> pila){
+        return entrada;
+    } */
+
+    private Stack<String> meter_produccion(Stack<String> pila, String puntero){
         String top = pila.get(pila.size()-1);
         pila.pop();
         switch (top){
@@ -66,12 +102,76 @@ public class Automata_pila {
                 pila.push("struct");
                 break;
             case "Name":
-                pila.push("RLI");
-                pila.push("LI");
+                if (validarOr(entrada.get(1))){
+                    pila.push("RLI");
+                    pila.push("__");
+                } else {
+                    pila.push("RLI");
+                    pila.push("LI");
+                }
                 break;
-            default: pila.clear();
+            case "LI":
+                if (expresionLetra.matcher(puntero.substring(0,1)).find()) {
+                    pila.push(puntero.substring(0,1));
+                    aAux = puntero.substring(0,1);
+                    auxPun = false;
+                }
+                    break;
+            case "RLI":
+                Pattern terminales = Pattern.compile("(_|^[a-z0-9])");
+                if (terminales.matcher(puntero).find()){
+                    System.out.println("Entro letra");
+                    pila.push(puntero);
+                    auxPun = true;
+                } /*else if (expresionNum.matcher(puntero).find()){
+                    System.out.println("Entro numero");
+                    pila.push("RLI");
+                    pila.push("D");
+                } else if (puntero.equals("_")){
+                    System.out.println("Entro guion");
+                    pila.push("RLI");
+                    pila.push("_");
+                }*/
+                break;
+            /*case "LI":
+                pila.push("a");
+                break;
+            case "RLI":
+                if (puntero.equals(" ")){
+                    pila.push("abc");
+                }
+                break;*/
+            case "LA":
+                pila.push("{");
+                break;
+            case "IND":
+                break;
+            case "LC":
+                pila.push("}");
+                break;
+            default:
+                System.out.println("default");
+                pila.clear();
         }
         return pila;
+    }
+
+    private void mostrar_pila(Stack<String> pila){
+        System.out.println("--------------------------------------------");
+
+        for (int i = pila.size(); i>0; i--){
+            System.out.println(pila.get(i-1));
+        }
+        System.out.println("--------------------------------------------");
+    }
+
+    private boolean validarOr (String g){
+        boolean status_aux = false;
+        if(g.substring(0,2).equals("__")) {
+            status_aux = true;
+        }
+
+        return status_aux;
     }
 
     public boolean isStatus() {
--- src/main/java/com/example/structure_automata_pila/Controller.java
+++ src/main/java/com/example/structure_automata_pila/Controller.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,7 +29,16 @@ public class Controller {
             labelMensaje.setText("Validando cadena");
             System.out.println(cadena + "\n");
 
-            Automata_pila automata_pila = new Automata_pila(cadena);
+            ArrayList <String> entrada = separarTexto(cadena);
+
+
+            /*System.out.println("-----------------------Aqui1-----------------------------");
+            cortar(cadena);
+
+            System.out.println("-----------------------Aqui2-----------------------------\n");
+            ArrayList <String> a = separarTexto(cadena);*/
+
+            Automata_pila automata_pila = new Automata_pila(entrada);
             automata_pila.validar_entrada();
 
         } else {
@@ -44,4 +54,108 @@ public class Controller {
         labelMensaje.setText("");
         textArea.clear();
     }
+
+    static ArrayList<String> separarTexto(String texto) {
+        int cantidad = 0;
+        //boolean valid = false;
+        ArrayList<String> structSplit = new ArrayList<>();
+        // Map<Boolean,ArrayList<String>> validarStruct = new HashMap<Boolean,ArrayList<String>>();
+        // struct
+        structSplit.add(texto.substring(0, 6));
+
+        // nombre de la struct
+        int vali = 0;
+        for (int i = 6; i < texto.length(); i++) {
+            if (!texto.substring(i, i + 1).equals("{")) {
+                vali++;
+
+            } else {
+                break;
+            }
+        }
+        String nombreStruct = texto.substring(6, 6 + vali);
+        structSplit.add(nombreStruct.trim());
+
+        // llave de apertura
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto.charAt(i) == '{') {
+                System.out.println(texto.substring(i, i + 1));
+                structSplit.add(texto.substring(i, i+1));
+                cantidad = i;
+                break;
+            }
+        }
+
+        // atributos
+        String atributos = texto.substring(cantidad + 1, texto.length() - 1).trim();
+        if(atributos.contains(",")){
+            String[] atributosCorte = atributos.split(",");
+            for (String atributo : atributosCorte) {
+                //System.out.println(atributo);
+                if (atributo.contains(":")){
+                    String[] at = atributo.split(":");
+                    structSplit.add(at[0].trim());
+                    structSplit.add(":");
+                    structSplit.add(at[1].trim());
+                }
+
+            }
+        }else if (atributos.contains(" ")){
+            String[] atributosCorte = atributos.split(" ");
+            for (String atributo : atributosCorte) {
+                //System.out.println(atributo);
+                structSplit.add(atributo.trim());
+            }
+        }else{
+            structSplit.add("e");
+        }
+
+        //llave de cierre
+        String llaveCierre = texto.substring(texto.length() - 1, texto.length()).trim();
+        structSplit.add(llaveCierre);
+
+        return structSplit;
+
+    }
+
+    /*void cortar(String texto){
+        int con = 0;
+        int cantidad = 0;
+        while(con!=5){
+            if (con==0) {
+                System.out.println(texto.substring(0, 6));
+                con=1;
+            }else if (con==1){
+                int vali =0 ;
+                for (int i = 6; i < texto.length(); i++) {
+                    if(!texto.substring(i,i+1).equals("{")){
+                        vali++;
+                    }else{
+                        break;
+                    }
+                }
+                System.out.println(texto.substring(6, 6+vali));
+                con=2;
+            }else if (con==2){
+                for (int i = 0; i < texto.length(); i++) {
+                    if (texto.charAt(i) == '{'){
+                        System.out.println(texto.substring(i, i+1));
+                        cantidad=i;
+                        break;
+                    }
+                }
+                con=3;
+            }else if (con==3){
+                String[] atributosCorte = texto.substring(cantidad+1, texto.length()-1).split(",");
+                System.out.println("Atributos");
+                for (String string : atributosCorte) {
+                    System.out.println(string);
+                }
+                con=4;
+            }else if (con==4){
+                System.out.println(texto.substring(texto.length()-1, texto.length()));
+                con=5;
+            }
+        }
+    } */
 }
